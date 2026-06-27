05aa7bbd494b29ae84814a46d1f41c6525ca4329
Sagar Bisht <sagarbishtz589@gmail.com>
2026-06-28 03:36:12 +0530

    done-789

--- components/VVSaarthiSidebar.tsx
+++ components/VVSaarthiSidebar.tsx
@@ -194,13 +194,13 @@ export function VVSaarthiSidebar() {
     <>
       {isOpen && (
         <div
-          className="fixed inset-0 bg-black/40 z-40 transition-opacity lg:bg-black/20"
+          className="fixed inset-0 bg-black/40 z-[110] transition-opacity lg:bg-black/20"
           onClick={close}
         />
       )}
 
       <div
-        className={`fixed top-0 right-0 h-full w-full sm:w-[420px] bg-white shadow-2xl z-50 transform transition-transform duration-300 ease-in-out flex flex-col ${isOpen ? 'translate-x-0' : 'translate-x-full'
+        className={`fixed top-0 right-0 h-full w-full sm:w-[420px] bg-white shadow-2xl z-[120] transform transition-transform duration-300 ease-in-out flex flex-col ${isOpen ? 'translate-x-0' : 'translate-x-full'
           }`}
       >
         {/* Header */}
